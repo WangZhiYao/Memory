0194e87844b41f3315f0be02be3cc76db03b540d
WangZhiYao <393893729@qq.com>
2017-11-23 14:54:27 +0800

    update some interface /2017-11-23

---
+++ app/src/main/java/space/levan/memory/ui/activities/SearchActivity.java
@@ -0,0 +1,23 @@
+package space.levan.memory.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import space.levan.memory.R;
+
+/**
+ * File description
+ *
+ * @author WangZhiYao
+ * @date 2017/11/23
+ */
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
